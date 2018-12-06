9a64bf349b4522573aff398b6632e45b7910adf7
fpvks <rephanoise@naver.com>
2018-12-06 17:33:46 +0900

    sql char 범위 증가

--- src/main/java/kr/or/yi/java_web_male/dto/CategoryB.java
+++ src/main/java/kr/or/yi/java_web_male/dto/CategoryB.java
@@ -3,31 +3,37 @@ package kr.or.yi.java_web_male.dto;
 public class CategoryB {
 	private int categorynoB;
 	private String themaB;
-	
+
 	public int getCategorynoB() {
 		return categorynoB;
 	}
+
 	public void setCategorynoB(int categorynoB) {
 		this.categorynoB = categorynoB;
 	}
+
 	public String getThemaB() {
 		return themaB;
 	}
+
 	public void setThemaB(String themaB) {
 		this.themaB = themaB;
 	}
+
 	@Override
 	public String toString() {
 		return "CategoryB [categorynoB=" + categorynoB + ", themaB=" + themaB + "]";
 	}
+
 	public CategoryB() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+
 	public CategoryB(int categorynoB, String themaB) {
 		super();
 		this.categorynoB = categorynoB;
 		this.themaB = themaB;
 	}
-	
+
 }
--- src/main/java/kr/or/yi/java_web_male/dto/CategoryM.java
+++ src/main/java/kr/or/yi/java_web_male/dto/CategoryM.java
@@ -4,34 +4,41 @@ public class CategoryM {
 	private int categorynoM;
 	private String themaM;
 	private CategoryB categorynoB;
-	
+
 	public int getCategorynoM() {
 		return categorynoM;
 	}
+
 	public void setCategorynoM(int categorynoM) {
 		this.categorynoM = categorynoM;
 	}
+
 	public String getThemaM() {
 		return themaM;
 	}
+
 	public void setThemaM(String themaM) {
 		this.themaM = themaM;
 	}
+
 	public CategoryB getCategorynoB() {
 		return categorynoB;
 	}
+
 	public void setCategorynoB(CategoryB categorynoB) {
 		this.categorynoB = categorynoB;
 	}
+
 	@Override
 	public String toString() {
 		return "CategoryM [categorynoM=" + categorynoM + ", themaM=" + themaM + ", categorynoB=" + categorynoB + "]";
 	}
+
 	public CategoryM(int categorynoM, String themaM, CategoryB categorynoB) {
 		super();
 		this.categorynoM = categorynoM;
 		this.themaM = themaM;
 		this.categorynoB = categorynoB;
 	}
-	
+
 }
