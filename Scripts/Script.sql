83aacae48f77ea9cf618b21b8e23da62530331fc
lenovo <superdongte@naver.com>
2018-12-07 13:55:16 +0900

    컬럼식별(korName, engName)수정

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -4,6 +4,8 @@ insert into category_b values('0','총류');
 SELECT *
 from member;
 
+
+
 select member_no, password, kor_name, eng_name, phone, jumin, email, address, photo, admin, memo
 from member;
 
