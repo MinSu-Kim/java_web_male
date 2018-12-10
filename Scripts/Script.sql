3f750a32469767f769b01262ed6b665b67cf25e7
lenovo <superdongte@naver.com>
2018-12-10 16:36:13 +0900

    조인구문 중

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -2,7 +2,7 @@ insert into category_b values('0','총류');
 insert into category_m values('0','0','총류');
 insert into category_s values('0','0','0','총류');
 insert into publisher values('P001','영남인제교육원');
-
+insert into book_rental_info values(3,'2018-12-10','2018-12-17',null,'2','00001');
 
 SELECT *
 from member;
@@ -15,7 +15,10 @@ call search_name("김동준");
 call search_phone("01022306796");
 call search_jumin("921012");
 
-
+select kor_name, m.member_no, title,rental_date, return_date, return_schedule
+from member m left outer join book_rental_info r on m.member_no = r.rental_no
+	left outer join book b on b.book_code =  r.rental_no
+	where kor_name="김동준";
 select member_no, password, kor_name, eng_name, phone, jumin, email, address, photo, admin, memo
 from member;
 
@@ -26,7 +29,8 @@ VALUES('00001', 0, 'P001', '김재영', '김재영', '자바의 정석', 40000, 
 >>>>>>> branch 'master' of https://github.com/MinSu-Kim/java_web_male.git
 
 SELECT * FROM book;
-
+select * from book_rental_info;
+select * from publisher;
 
 SELECT cate_m_no, cate_b_no, cate_m_name
 		FROM proj_library.category_m
--- src/main/java/kr/or/yi/java_web_male/service/MemberUIService.java
+++ src/main/java/kr/or/yi/java_web_male/service/MemberUIService.java
@@ -1,9 +1,18 @@
 package kr.or.yi.java_web_male.service;
 
+import java.util.List;
+
 import kr.or.yi.java_web_male.dao.MemberMapper;
+import kr.or.yi.java_web_male.dao.MemberMapperImpl;
+import kr.or.yi.java_web_male.dto.Member;
 
 public class MemberUIService {
-		private MemberMapper memberMapper;
+	
+	private MemberMapper dao = new MemberMapperImpl();
+	
+	public List<Member> selectMemberByAll(){
+		return dao.selectMemberByAll();
+	}
 		
 	
 }
